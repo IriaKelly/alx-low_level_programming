f7f0acc1106573f788efa2328e31ea461d978b04
IriaKelly <esezoborkelly@gmail.com>
2023-08-18 14:42:28 -0700

    Prints a triangle of given size using # character

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_triangle - Prints a triangle of given size using #
+ * @size: Size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = size - i; j > 0; j--)
+		{
+			_putchar(' ');
+		}
+		for (k = 1; k <= i; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
