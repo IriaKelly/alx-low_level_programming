80b01908586f3246a43cd985b8bb69a4f60bd95b
IriaKelly <esezoborkelly@gmail.com>
2023-08-18 11:40:50 -0700

    prints numbers from 0 to 9 (excluding 2 and 4)

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
- * print_numbers - prints numbers from 0 to 9 (excluding 2 and 4)
+ * print_most_numbers - prints numbers from 0 to 9 (excluding 2 and 4)
  */
 void print_most_numbers(void)
 {
