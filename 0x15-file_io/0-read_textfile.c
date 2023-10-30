3a1c0847b835e94f96835702ee2cac527286c680
IriaKelly <esezoborkelly@gmail.com>
2023-10-30 10:57:06 -0700

    Tasks for 0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,29 +2,32 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads and prints to stdout
  * @filename: pointer to the filename
  * @letters: number of letters
  * Return: the actual number of letters it could read and print
  */
-ssize_t read_textfile(const char *filename, size_t letters) {
-    FILE *file = fopen(filename, "r");
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *file = fopen(filename, "r");
 
-    if (file == NULL) {
-        printf("Oops! Something went wrong while opening the file.\n");
-        return 0;
-    }
+	if (file == NULL)
+	{
+		printf("Oops! Something went wrong while opening the file.\n");
+		return (0);
+	}
 
-    char buffer[letters];
-    ssize_t read_letters = fread(buffer, 1, letters, file);
-    buffer[read_letters] = '\0';
+	char buffer[letters];
+	ssize_t read_letters = fread(buffer, 1, letters, file);
 
-    for (ssize_t i = 0; i < read_letters; i++) {
-        _putchar(buffer[i]);
-    }
+	buffer[read_letters] = '\0';
 
-    fclose(file);
+	for (ssize_t i = 0; i < read_letters; i++)
+	{
+		_putchar(buffer[i]);
+	}
 
-    return read_letters;
+	fclose(file);
+
+	return (read_letters);
 }
-
